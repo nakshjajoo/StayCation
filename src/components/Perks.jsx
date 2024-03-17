121b66714fa3645a355a0880dce39907e97bcea1
Naksh Jajoo <jajoonaksh01@gmail.com>
2024-03-17 15:52:23 -0500

    initial commit

---
+++ src/components/Perks.jsx
@@ -0,0 +1,58 @@
+const Perks = ({selected, onChange}) => {
+
+  const handleCbClick = (e) => {
+    const {checked, name} = e.target;
+    if (checked) {
+      onChange([...selected, name]); 
+    } else {
+      onChange([...selected.filter(selectedName => selectedName !== name)]);
+    }
+  }
+
+	return (
+		<>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('wifi')} name="wifi" onChange={(handleCbClick)} />
+				<span>Wi-Fi</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('parking')} name="parking" onChange={(handleCbClick)} />
+				<span>Free Parking</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('tv')} name="tv" onChange={(handleCbClick)} />
+				<span>TV</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('pets')} name="pets" onChange={(handleCbClick)} />
+				<span>Pets allowed</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('kitchen')} name="kitchen" onChange={(handleCbClick)} />
+				<span>Kitchen and basic appliances</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('heatcool')} name="heatcool" onChange={(handleCbClick)} />
+				<span>Heating and cooling systems</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('outdoor')} name="outdoor" onChange={(handleCbClick)} />
+				<span>Outdoor furniture, dining area, BBQ, fire pit</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('bathtub')} name="bathtub" onChange={(handleCbClick)} />
+				<span>Bathtub</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('laundry')} name="laundry" onChange={(handleCbClick)} />
+				<span>Laundry</span>
+			</label>
+			<label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+				<input type="checkbox" checked={selected.includes('recreation')} name="recreation" onChange={(handleCbClick)} />
+				<span>Recreational activities(Pool, Ping Pong, etc.)</span>
+			</label>
+		</>
+	);
+};
+
+export default Perks;
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,51 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../UserContext";
+import { Navigate, useParams } from "react-router-dom";
+import axios from "axios";
+import PlacesPage from "./PlacesPage";
+import AccountNav from "../components/AccountNav";
+
+const ProfilePage = () => {
+	const [redirect, setRedirect] = useState(null);
+	const { user, setUser, ready } = useContext(UserContext);
+	let { subpage } = useParams();
+
+	const logout = async () => {
+		await axios.post("/logout");
+		setRedirect("/");
+		setUser(null);
+	};
+
+	if (!ready) {
+		return "Loading...";
+	}
+
+	if (ready && !user && !redirect) {
+		return <Navigate to={"/login"} />;
+	}
+
+	//this will correspond to the case of my profile tab since the param for my profile is undefined
+	if (subpage === undefined) {
+		subpage = "profile";
+	}
+
+	if (redirect) {
+		return <Navigate to={redirect} />;
+	}
+	return (
+		<div>
+			<AccountNav />
+			{subpage === "profile" && (
+				<div className="text-center max-w-lg mx-auto">
+					Logged in as {user.name} ({user.email}) <br />
+					<button onClick={logout} className="primary max-w-sm mt-2">
+						Logout
+					</button>
+				</div>
+			)}
+			{subpage === "places" && <PlacesPage />}
+		</div>
+	);
+};
+
+export default ProfilePage;
