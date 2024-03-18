99a7113426ab0d94f5b8919377302a0f1999032f
Naksh Jajoo <jajoonaksh01@gmail.com>
2024-03-18 00:35:36 -0500

    first commit

--- api/index.js
+++ api/index.js
@@ -225,4 +225,4 @@ app.get('/bookings', async (req, res) => {
     res.json( await Booking.find({user: userData.id}).populate('place'));
 })
 
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
