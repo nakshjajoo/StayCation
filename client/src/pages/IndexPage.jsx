a08b81ec6e6ac377696ced60a434338ba5603078
Naksh Jajoo <jajoonaksh01@gmail.com>
2024-04-13 00:00:05 -0500

    first commit

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -27,17 +27,13 @@ const IndexPage = () => {
 
 	return (
 		<div className="mt-20 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-			{places.length > 0 &&
-				places.map((place) => (
+			{places.length > 0 &&	places.map((place) => (
 					<Link to={"/place/" + place._id} key={place._id}>
 						<div className="bg-gray-500 mb-2 rounded-2xl flex overflow-hidden">
 							{place.photos?.[0] && (
 								<img
 									className="rounded-2xl aspect-square object-cover transition-all hover:opacity-80 hover:scale-105 duration-150"
-									src={
-										`${import.meta.env.VITE_BASE_URL}/uploads/` +
-										place.photos?.[0]
-									}
+									src={`${import.meta.env.VITE_BASE_URL}/uploads/` +place.photos?.[0]}
 									alt="img"
 								/>
 							)}
